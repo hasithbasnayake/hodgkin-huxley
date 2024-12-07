79184ebb585985c1b5a3c14f6e8c4befa32d0c74
hasithbasnayake <hasith@ucsb.edu>
2024-12-07 12:14:46 -0800

    Added class scaffolding of neuron for later implementation

--- main.py
+++ main.py
@@ -2,3 +2,18 @@
 # pip freeze > requirements.txt You can export a list of all installed packages
 # pip install -r requirements.txt to install from a requirements file
 # pip list to list all packages
+
+import numpy as np
+import matplot.pyplot as plt 
+
+# The hogkin-huxley neuronal model allows one to simulate a neuron through an approximation of voltage gated dynamics
+# It is one of the most computationally equivalent biological models of the neuron, which comes as at a computational cost 
+
+class Neuron():
+    def __init__(self):
+    
+    def __str__():
+    
+    def runNeuron(self):
+
+# Created class scaffolding for later implementation

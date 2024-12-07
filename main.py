f8dc638a8bd2e06142d1b0e64b00d76be5a9a360
hasithbasnayake <hasith@ucsb.edu>
2024-12-07 15:55:37 -0800

    Gating variables

--- main.py
+++ main.py
@@ -4,27 +4,38 @@
 # pip list to list all packages
 
 import numpy as np
-import matplot.pyplot as plt 
 
 # The hogkin-huxley neuronal model allows one to simulate a neuron through an approximation of voltage gated dynamics
 # It is one of the most computationally equivalent biological models of the neuron, which comes as at a computational cost 
 
 class Neuron():
-    def __init__(self, n, m, h, c_m, I_e, g_Na, g_K, g_L, V_m, E_Na, E_K, E_L):
-        n = self.n
-        m = self.m 
-        h = self.h 
-        c_m = self.c_m
-        I_e = self.I_e
-        g_Na = self.g_Na
-        g_K = self.g_K
-        g_L = self.g_L
-        V_m = self.V_m
-        E_Na = self.E_Na
-        E_K = self.E_K
-        E_L = self.E_L  
-    def __str__():
+    def __init__(self, g_Leak, g_Na, g_K, v_Na, v_K, v_Leak, C_m, Vm):
+        self.g_Leak = g_Leak
+        self.g_Na = g_Na
+        self.g_K = g_K
+        self.v_Na = v_Na
+        self.v_K = v_K
+        self.v_Leak = v_Leak
+        self.C_m = C_m
+
+        self.Vm = Vm
+
+        alpha_m = (.1 * (Vm + 40)) / (1 - np.exp(-.1 * (Vm + 40)))
+        beta_m = 4 * np.exp(-.0556 * (Vm + 65))
+        alpha_h = .07 * np.exp(-.05 * (Vm + 65))
+        beta_h = 1 / (1 + np.exp(-.1 * (Vm + 35)))
+        alpha_n = 0.01 * (Vm + 55) / (1 - np.exp(-(Vm + 55) / 10))
+        beta_n = .125 * np.exp(-.0125 * (Vm + 65))
     
-    def runNeuron(self):
+    def __str__(self):
+        return (f"Neuron: g_Leak={self.g_Leak}, g_Na={self.g_Na}, g_K={self.g_K}, "
+                f"v_Na={self.v_Na}, v_K={self.v_K}, v_Leak={self.v_Leak}, C_m={self.C_m}")
+
+    
+
 
 # Created class scaffolding for later implementation
+
+Neuron = Neuron(g_Leak= 0.3, g_Na = 120, g_K = 36, v_Na = 45, v_K = -82, v_Leak = -60, C_m = 1)
+
+print(Neuron)

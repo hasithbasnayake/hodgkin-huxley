0a1e4fa560cab72fb7ed33dd65c579308b6a8d05
hasithbasnayake <hasith@ucsb.edu>
2024-12-07 12:07:13 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,4 @@
+# .\env\Scripts\activate to activate virtual environment
+# pip freeze > requirements.txt You can export a list of all installed packages
+# pip install -r requirements.txt to install from a requirements file
+# pip list to list all packages

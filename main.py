5ed187c40a418e04cada08211eeeea4516229170
hasithbasnayake <hasith@ucsb.edu>
2024-12-07 12:17:26 -0800

    added variables

--- main.py
+++ main.py
@@ -10,8 +10,19 @@ import matplot.pyplot as plt
 # It is one of the most computationally equivalent biological models of the neuron, which comes as at a computational cost 
 
 class Neuron():
-    def __init__(self):
-    
+    def __init__(self, n, m, h, c_m, I_e, g_Na, g_K, g_L, V_m, E_Na, E_K, E_L):
+        n = self.n
+        m = self.m 
+        h = self.h 
+        c_m = self.c_m
+        I_e = self.I_e
+        g_Na = self.g_Na
+        g_K = self.g_K
+        g_L = self.g_L
+        V_m = self.V_m
+        E_Na = self.E_Na
+        E_K = self.E_K
+        E_L = self.E_L  
     def __str__():
     
     def runNeuron(self):

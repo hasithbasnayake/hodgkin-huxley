01fadf3e6ae11e18ed742154cf4b4d7c7dda7886
hasithbasnayake <hasith@ucsb.edu>
2024-12-07 16:29:23 -0800

    Succesfully producing spikes with current initialized values and injected current

--- main.py
+++ main.py
@@ -3,39 +3,26 @@
 # pip install -r requirements.txt to install from a requirements file
 # pip list to list all packages
 
+from func import *
 import numpy as np
-
-# The hogkin-huxley neuronal model allows one to simulate a neuron through an approximation of voltage gated dynamics
-# It is one of the most computationally equivalent biological models of the neuron, which comes as at a computational cost 
-
-class Neuron():
-    def __init__(self, g_Leak, g_Na, g_K, v_Na, v_K, v_Leak, C_m, Vm):
-        self.g_Leak = g_Leak
-        self.g_Na = g_Na
-        self.g_K = g_K
-        self.v_Na = v_Na
-        self.v_K = v_K
-        self.v_Leak = v_Leak
-        self.C_m = C_m
-
-        self.Vm = Vm
-
-        alpha_m = (.1 * (Vm + 40)) / (1 - np.exp(-.1 * (Vm + 40)))
-        beta_m = 4 * np.exp(-.0556 * (Vm + 65))
-        alpha_h = .07 * np.exp(-.05 * (Vm + 65))
-        beta_h = 1 / (1 + np.exp(-.1 * (Vm + 35)))
-        alpha_n = 0.01 * (Vm + 55) / (1 - np.exp(-(Vm + 55) / 10))
-        beta_n = .125 * np.exp(-.0125 * (Vm + 65))
-    
-    def __str__(self):
-        return (f"Neuron: g_Leak={self.g_Leak}, g_Na={self.g_Na}, g_K={self.g_K}, "
-                f"v_Na={self.v_Na}, v_K={self.v_K}, v_Leak={self.v_Leak}, C_m={self.C_m}")
-
-    
-
-
-# Created class scaffolding for later implementation
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+from scipy.interpolate import interp1d
 
 Neuron = Neuron(g_Leak= 0.3, g_Na = 120, g_K = 36, v_Na = 45, v_K = -82, v_Leak = -60, C_m = 1)
+
+t = np.arange(0,300,1)
+I_input = np.zeros(t.shape, dtype = t.dtype)
+I_input[(t>50) & (t<100)] = 10
+I_input[(t>150) & (t<200)] = 30
+
+init_vals = [-65.0, 0.05, 0.32, 0.6]
+I_interp = interp1d(t, I_input, fill_value='extrapolate')
+C = odeint(C_dvdt, init_vals, t, args=(I_interp, Neuron.g_Na, Neuron.v_Na, Neuron.g_K, Neuron.v_K, Neuron.g_Leak, Neuron.v_Leak))
+
+V = C[:,0]
+
+plt.plot(t, V, 'y')
+plt.show()
 
 print(Neuron)
